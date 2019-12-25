440435676522cb6ca03ca409e1272b4b674bef50
zarksalman <zark.salman@gmail.com>
2019-12-25 17:14:36 +0500

    hand over to bilal with ui changes

--- app/src/main/java/com/textile/markeet/helpers/AppUtils.java
+++
@@ -1,9 +0,0 @@
-package com.textile.markeet.helpers;
-
-import android.widget.Toast;
-
-public class AppUtils {
-
-    public static Toast mToast;
-
-}
---
+++ app/src/main/java/com/textile/markeet/helpers/AppUtils.kt
@@ -0,0 +1,7 @@
+package com.textile.markeet.helpers
+
+import android.widget.Toast
+
+object AppUtils {
+    var mToast: Toast? = null
+}
